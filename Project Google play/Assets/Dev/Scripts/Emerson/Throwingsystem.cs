b2d522eb1f89807c95ffd5911ca351e026d1ce4f
Emerson <emerson_10@hotmail.nl>
2019-11-15 16:08:11 +0100

    fixed

--- Project Google play/Assets/Dev/Scripts/Emerson/Throwingsystem.cs
+++ Project Google play/Assets/Dev/Scripts/Emerson/Throwingsystem.cs
@@ -1,30 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Throwingsystem : MonoBehaviour
 {
     private BallPooling ballPool;
     Vector2 m_startPos, m_endPos;
+    Vector2 m_Debug;
     Vector3 m_direction;
     float m_touchTimeStart, m_touchTimeFinish, m_timeInterval;
 
     public float forwardForce;
     public float sideForce;
     [SerializeField] private Rigidbody ballRigidbody;
+    [SerializeField] private TMP_Text m_DebugText;
 
+#if UNITY_EDITOR
     private void Start()
     {
         ballPool = FindObjectOfType<BallPooling>();
         StartCoroutine(ThrowPreocedureAsync());
     }
-
+#elif UNITY_ANDROID
+private void Start()
+    {
+        ballPool = FindObjectOfType<BallPooling>();
+        StartCoroutine(ThrowPreocedureAsync());
+    }
+#endif
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene(0);
-        }       
+        }
+
+        m_DebugText.text = m_Debug.x + "," + m_Debug.y;
     }
 
     private IEnumerator ThrowPreocedureAsync()
@@ -42,6 +55,7 @@ public class Throwingsystem : MonoBehaviour
         Vector2 originalTouchPosition = Input.mousePosition;
         float startTime = Time.time;
 
+        m_Debug = originalTouchPosition;
         while (!Input.GetMouseButtonUp(0))
         {
             yield return null;
